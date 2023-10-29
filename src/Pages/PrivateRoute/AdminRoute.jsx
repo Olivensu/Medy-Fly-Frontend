7086a31f633440f6106b2478a2f574f984b00a39
OliveNsu <tasbiul.hasan@northsouth.edu>
2023-10-30 00:16:48 +0600

    admin route

--- src/Pages/PrivateRoute/AdminRoute.jsx
+++ src/Pages/PrivateRoute/AdminRoute.jsx
@@ -12,37 +12,26 @@ const AdminRoute = ({children}) => {
   const [isAdmin, setIsAdmin] = useState(false);
 
     useEffect(() => {
-      //   fetch(`${process.env.REACT_APP_API_BASE_URL}/users/${user?.email}`)
-      //   .then(res=>res.json())
-      //   .then(data=> {
-      //     setUserinfo(data.isAdmin)
-      //     console.log(data.isAdmin)
-      //   })
-
-        //const timer = setTimeout(() => {
-      // Make an API call to your backend to check if the user is an admin
-      // Replace 'apiEndpointForAdminCheck' with your actual API endpoint
-      fetch(`${import.meta.env.VITE_BACKEND_URL}/api/users/${user?.email}`)
+      if(user){
+        fetch(`${import.meta.env.VITE_BACKEND_URL}/api/users/${user?.email}`)
         .then((response) => response.json())
         .then((data) => {
-          setIsAdmin(data.payload.user.isAdmin); // Set isAdmin based on the response from the server
-          setIsLoading(false); // Update isLoading to false after the check is complete
+          setIsAdmin(data.payload.user.isAdmin);
+          setIsLoading(false); 
           console.log(isAdmin)
         })
         .catch((error) => {
           console.error('Error checking admin status:', error);
           setIsLoading(false);
         });
-    //}, 8000);
-
-    //return () => clearTimeout(timer);
-      }, [])
+      }
+      }, [user])
 
     
       if(loading || isLoading){
         return <Loading></Loading>
       }
-    if(isAdmin!== "true"){
+    if(!isAdmin){
         return <Navigate to='/login' state={{from: location}}></Navigate>
     }
     return children;
